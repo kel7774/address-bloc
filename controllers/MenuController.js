9694e91d0d713dac68c3362d826b3e627cbf2573
Kelli Landry <kellilandry@Kellis-Mac-mini.local>
2018-11-05 14:05:55 -0600

    Tests failing. Add column error.

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -10,6 +10,7 @@ module.exports = class MenuController {
                 message: "Please choose from an option below: ",
                 choices: [
                     "Add new contact",
+                    "View all contacts",
                     "Exit"
                 ]
             }
@@ -23,6 +24,9 @@ module.exports = class MenuController {
             switch(response.mainMenuChoice){
                 case "Add new contact":
                     this.addContact();
+                    break;
+                case "View all contacts":
+                    this.getContacts();
                     break;
                 case "Exit":
                     this.exit();
@@ -60,5 +64,23 @@ module.exports = class MenuController {
 
     getContactCount(){
         return this.contacts.length;
+    }
+
+    getContacts() {
+        this.clear();
+        this.book.getContacts().then((contacts) => {
+            for(let contact of contacts){
+                console.log(`
+                name: ${contact.name}
+                phone number: ${contact.phone}
+                email: ${contact.email}
+                ------------------`
+            );
+        }
+            this.main();
+        }).catch((err) => {
+            console.log(err);
+            this.main();
+        });
     }
 }
